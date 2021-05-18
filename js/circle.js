f3636c5fe86e799cc9607d8c5425cb7790c1f2b0
DinhNPVy <4501104282@student.hcmue.edu.vn>
2021-05-18 20:15:08 +0700

    circle

---
+++ js/circle.js
@@ -0,0 +1,8 @@
+let radius = +prompt("Nhập bán kính: ");
+ pi = 3.14;
+
+let C = 2 * radius * pi;
+
+document.write("Chu vi hình tròn là: " + C + "<br>");
+let S = radius * radius * pi;
+document.write("Diện tích hình tròn là hình tròn là: " + S);
