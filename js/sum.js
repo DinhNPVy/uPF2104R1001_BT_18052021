1d8c13ecd9d695dda496613f180bfb7f181ceb4c
DinhNPVy <4501104282@student.hcmue.edu.vn>
2021-05-18 19:25:35 +0700

    sum

---
+++ js/sum.js
@@ -0,0 +1,13 @@
+// phan tich bai toan
+// input 2 so a va b
+// ouput: tong 2 so a va b
+// giai thuat:
+/*
+  b1: nhap du lieu tu ban phim gan vao 2 bien a va b
+  b2: tao bien sum = a + b
+  b3: in ket qua
+*/
+let a = +prompt("Nhap so a: ");
+let b = +prompt("Nhap so b: ");
+sum = a + b;
+document.write('Tong hai so la: ' + sum);
