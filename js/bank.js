98086d88d79ef0d793598e66435df2bbd3ebadf3
DinhNPVy <4501104282@student.hcmue.edu.vn>
2021-05-18 20:01:03 +0700

    upload

--- js/bank.js
+++ js/bank.js
@@ -1,7 +1,7 @@
 let  lend = +prompt("Nhập số tiền vay: ");
 let i_rate = parseFloat(prompt("Nhập lãi suất: "));
-let n = +prompt("Nhập số năm: ");
+let year = +prompt("Nhập số năm: ");
 let irate = (i_rate/100)/n;
-soTienLai = lend * irate;
+Amountofprofit = lend * irate;
 
-document.write(soTienLai + " triệu đồng");
+document.write(Amountofprofit + " triệu đồng");
